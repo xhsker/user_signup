ba28c06649125e97d432060309880ca92c4d2bb9
Tom Lake <xhsker@sbcglobal.net>
2018-09-30 16:23:05 -0500

    final changes to code

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request,redirect, render_template
+import cgi
+import os
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
 
-#@app.route('/')
-#def index():
-#    return render_template('userid.html')
+@app.route("/")
+def index():
+    return render_template('userid.html')
 
-
-@app.route("/", methods=['POST', 'GET'])
-def input_verification():
-
-    #making sure to get the errors set to an empty string to give the user an error
-    #if they don't fill out form completely
+@app.route("/", methods=['POST'])
+def validate_inputs():
 
     user_name = ""
     user_email = ""
@@ -21,8 +18,6 @@ def input_verification():
     error_password = ""
     error_verify_password = ""
     error_email = ""
-
-    #makes sure to get a "POST" request from the browser
 
     if request.method == 'POST':
         user_name = request.form['user_name']
@@ -49,28 +44,35 @@ def input_verification():
         for char in user_password:
             if char.isspace():
                 error_password = "Password cannot contain spaces"
-            else:
-                if (len(user_password)<3) or (len(user_password)>20):
-                    error_password = "Password must be 3 to 20 characters without spaces"
-
-        if not len(user_password):
-            error_password = "Pleae enter a valid password"
+            elif (len(user_password)<3) or (len(user_password)>20):
+                error_password = "Password must be 3 to 20 characters without spaces"
+            elif not len(user_password):
+                error_password = "Pleae enter a valid password"
 
         if user_password != verify_password:
             error_verify_password = "The passwords do not match"
 
         #test for a valid email entered by the user
-        if (user_email !="") and (not user_email('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-.+$', user_email)):
-            error_email = "Please enter a valid email"
-            user_email = ""
+        
+        if user_email == '':
+            error_email = ""
+        elif "@" not in user_email:
+            error_email = "This is not a valid email address"
+        elif "." not in user_email:
+            error_email = "This is not a valid email address"
+        elif " " in user_email:
+            error_email = "This is not a valid email address"
+        elif len(user_email)<3 or len(user_email)>20:
+            error_email = "This email is too long for our form"
 
-        #check each to see if there are errors
+
+        #check each to see if there are errors and render proper form
         if (not error_name) and (not error_password) and (not error_verify_password) and (not error_email):
             return render_template ('finalform.html', user_name = user_name)
         else:
-             render_template('userid.html', user_name = user_name, user_email = user_email, 
+            return render_template('userid.html', user_name = user_name, user_email = user_email, 
                             error_name = error_name, error_password = error_password, 
                             error_verify_password = error_verify_password, error_email = error_email)
 
-    app.run()
 
+app.run()
